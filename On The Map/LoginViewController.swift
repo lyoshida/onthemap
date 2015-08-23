5261b777d6409334f667fe3dc072add5dff8acb2
Luis Yoshida <luis.yoshida@gmail.com>
2015-08-23 15:56:20 -0300

    Imported assets. Login View layout.

---
+++ On The Map/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  loginViewController.swift
+//  On The Map
+//
+//  Created by Luis Yoshida on 8/23/15.
+//  Copyright (c) 2015 Msen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Sets visual layout
+        var gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.view.bounds
+        gradient.colors = [UIColor(red: 250.0/255.0, green: 172.0/255.0, blue: 135.0/255.0, alpha: 1.0).CGColor, UIColor(red: 247.0/255.0, green: 140.0/255.0, blue: 53.0/255.0, alpha: 1.0).CGColor]
+        self.view.layer.insertSublayer(gradient, atIndex: 0)
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
