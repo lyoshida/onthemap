5a3d75fdad261a92025a2ae8b094d4fa64759462
Luis Yoshida <luis.yoshida@gmail.com>
2015-08-29 16:52:16 -0300

    Put functions.

--- On The Map/OTMParseConvenience.swift
+++ On The Map/OTMParseConvenience.swift
@@ -82,6 +82,46 @@ extension OTMClient {
         
     }
     
+    func taskForParsePUTMethod(url: String, parameters: [String: AnyObject]?, jsonBody: [String: AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
+        
+        var urlString : String
+        // Build the URL
+        if let params = parameters {
+            urlString = "\(url)\(OTMClient.escapedParameters(params))"
+        } else {
+            urlString = "\(url)"
+        }
+        
+        let url = NSURL(string: urlString)
+        let request = NSMutableURLRequest(URL: url!)
+        var jsonifyError: NSError? = nil
+        
+        request.addValue(OTMClient.Constants.parseApplicationId, forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue(OTMClient.Constants.parseApiKey, forHTTPHeaderField: "X-Parse-REST-API-Key")
+        
+        // Request setup
+        request.HTTPMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(jsonBody, options: nil, error: &jsonifyError)
+        
+        // Make the request
+        let task = session.dataTaskWithRequest(request) {
+            data, response, downloadError in
+            
+            if let error = downloadError {
+                completionHandler(result: nil, error: downloadError)
+            } else {
+                OTMClient.parseJSONWithCompletionHandler(data, removeStart: false, completionHandler: completionHandler)
+            }
+        }
+        
+        task.resume()
+        
+        return task
+        
+    }
+    
     // Retrives the list of students, saves it to the self.studentList variable and return the result to the completionHandler.
     func getStudents(forceReload: Bool, completionHandler: (result: AnyObject?, errorString: NSError?) -> Void) {
         
@@ -128,6 +168,32 @@ extension OTMClient {
         ]
         
         let task = taskForParsePOSTMethod(url, parameters: nil, jsonBody: jsonBody) { result, error in
+            
+            if let error = error {
+                completionHandler(result: nil, errorString: error)
+            } else {
+                completionHandler(result: result, errorString: error)
+            }
+            
+        }
+    }
+    
+    func putUserLocation(objectID: String, location: String, link: String, latitude: String, longitude: String, completionHandler: (result: AnyObject?, errorString: NSError?) -> Void) {
+        
+        let url: String = "\(OTMClient.Constants.parseApiUrl)/\(objectID)"
+        let headerParams: [String: String] = OTMClient.Constants.headerParams
+
+        let jsonBody: [String: AnyObject] = [
+            "latitude": latitude,
+            "longitude": longitude,
+            "uniqueKey": self.userId!,
+            "firstName": self.userFirstName!,
+            "lastName": self.userLastName!,
+            "mapString": location,
+            "mediaUrl": link
+        ]
+        
+        let task = taskForParsePUTMethod(url, parameters: nil, jsonBody: jsonBody) { result, error in
             
             if let error = error {
                 completionHandler(result: nil, errorString: error)
